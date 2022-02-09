efc538b3ef878d0f6d06e31b5d1a393a4fe97423
gozld5153 <gozld5153@naver.com>
2022-02-09 16:21:02 +0900

    typescript type

--- type-script-type/app.ts
+++ type-script-type/app.ts
@@ -1,5 +1,5 @@
 function add(n1: number, n2: number){
-  // JS runtime 중 오류를 확인하는 것보다 TS로 개발하는 도중에 오류를 확인할 수 있어 쓸데없는 코드를 줄일 수 있다!
+  // JS runtime 중 오류를 확인하는 것보다 TS로 개발하는 도중에 오류를 확인할 수 있어 쓸데없는 코드를 줄일 수 있다!!
   // if(typeof n1 !== "number" || typeof n2 !== "number"){
   //   return new Error("wrong type")
   // }
